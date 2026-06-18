180fd9bdbe780e6eb05913d55ff23a6c3f2cf0c5
vinicius lima <vinicius.limalveslinux@gmail.com>
2026-06-18 20:58:02 -0300

    chore: estrutura inicial do projeto

---
+++ backend/src/main/java/br/com/smilecraft/api/SmilecraftApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.smilecraft.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmilecraftApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmilecraftApiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/smilecraft/api/SmilecraftApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.smilecraft.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmilecraftApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
